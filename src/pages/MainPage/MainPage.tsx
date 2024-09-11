46fecde5fcccc1248261a66879f8214922b97378
Sahara <sahara.ap13@gmail.com>
2024-09-11 18:41:57 +0300

    feat: adds presentation section

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react'; 
 
 import S from './_MainPage.module.scss'
+import { Presentation } from './components/Presentation';
 
 export const MainPage:React.FC = () => {
 
   return (
     <div className={S.wrapper}>
-      
+      <Presentation />
     </div>
  );
 }
